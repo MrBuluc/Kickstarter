823f6529fd3c499b9332e185864338c0f4f098e7
U180709061 <hakkicanbuluc@gmail.com>
2023-02-12 12:29:25 +0300

    added manager to campaigns

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,35 @@ import React, { Component } from "react";
 import { Card, Button } from "semantic-ui-react";
 import factory from "../ethereum/factory";
 import Layout from "../components/Layout";
+import web3 from "../ethereum/web3";
+import Campaign from "../ethereum/build/Campaign.json";
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
-    return { campaigns };
+    const campaignsManagerList = [];
+    for (let address of campaigns) {
+      const manager = await new web3.eth.Contract(
+        JSON.parse(Campaign.interface),
+        address
+      ).methods
+        .manager()
+        .call();
+      campaignsManagerList.push({ address, manager });
+    }
+    return { campaignsManagerList };
   }
 
   renderCampaigns() {
-    const items = this.props.campaigns.map((address) => {
+    const items = this.props.campaignsManagerList.map((campaignsManager) => {
       return {
-        header: address,
-        description: <a>View Campaign</a>,
+        header: campaignsManager.address,
+        description: (
+          <div>
+            <h4>Manager: {campaignsManager.manager}</h4>
+            <a>View Campaign</a>
+          </div>
+        ),
         fluid: true,
       };
     });
