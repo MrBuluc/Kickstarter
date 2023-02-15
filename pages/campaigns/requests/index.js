4dcd47895d3c1cf135097107fb5a0153c2769fd7
U180709061 <hakkicanbuluc@gmail.com>
2023-02-15 17:09:27 +0300

    added RequestRow to RequestIndex

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,23 +1,67 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
 import Layout from "../../../components/Layout";
+import getCampaign from "../../../ethereum/campaign";
+import RequestRow from "../../../components/RequestRow";
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query;
-    return { address };
+    const campaign = getCampaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+
+    const requests = await Promise.all(
+      Array(parseInt(requestCount))
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+
+    return { address, requests, requestCount };
+  }
+
+  renderRows() {
+    return this.props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          key={index}
+          request={request}
+          address={this.props.address}
+        />
+      );
+    });
   }
 
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
     return (
       <Layout>
         <h3>Requests</h3>
         <Link route={`/campaigns/${this.props.address}/requests/new`}>
           <a>
-            <Button positive>Add Request</Button>
+            <Button primary>Add Request</Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient Address</HeaderCell>
+              <HeaderCell>Yes Count</HeaderCell>
+              <HeaderCell>No Count</HeaderCell>
+              <HeaderCell>Rate of Votes</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Disapprove</HeaderCell>
+              <HeaderCell>Complete</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>{this.renderRows()}</Body>
+        </Table>
       </Layout>
     );
   }
